e9c908f597a9735c44e7cb1f3dbbb988653b3c91
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2025-11-05 11:05:13 -0500

    Initial Commit

---
+++ Calculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Calculator
+//
+//  Created by Gustavo Caldas de Souza on 2025-11-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
